ad0cb5b7535797a14925b5a0c3e04796d4c28082
Guillaume ROBIN <robinguillaume.pro@gmail.com>
2017-11-25 00:27:00 +0000

    Added: dev environment setup file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # alGoGen
 A framework coded in Go for Genetic Algorithm.
+
+## Set development environment up
+```
+$ . ./dev/setup.sh
+```
---
+++ dev/setup.sh
@@ -0,0 +1,5 @@
+# Define the go workspace directory for this project
+export GOPATH=`pwd`
+
+# Some alias to make life easier!
+alias build="go build"
