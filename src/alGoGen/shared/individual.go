351e19882bd59bc414e2b99336e958ce2b61bb4b
Guillaume ROBIN <robinguillaume.pro@gmail.com>
2017-11-26 21:40:10 +0000

    Updated: individual can now be sorted using type Individuals

--- src/alGoGen/shared/individual.go
+++ src/alGoGen/shared/individual.go
@@ -5,5 +5,21 @@ type Genotype interface {}
 type Individual interface {
 	Init(id int)
 	Id() int
-	GetGenotype() Genotype
+	Fitness() float64
+}
+
+type Individuals []*Individual
+
+func (a Individuals) Len() int {
+	return len(a)
+}
+
+func (a Individuals) Swap(i, j int) {
+	tmp := a[i]
+	a[i] = a[j]
+	a[j] = tmp
+}
+
+func (a Individuals) Less(i, j int) bool {
+	return (*a[i]).Fitness() < (*a[j]).Fitness()
 }
